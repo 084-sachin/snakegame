62c7d9d2b914b24d8a576c8bd3669ca8e7b509b4
084-sachin <sachinlalwani1234@gmail.com>
2021-11-27 11:59:34 +0530

    Update Code for deployment

--- frontend/index.js
+++ frontend/index.js
@@ -2,7 +2,7 @@ const BG_COLOUR = '#231f20';
 const SNAKE_COLOUR = '#0000FF';
 const FOOD_COLOUR = '#e66916';
 
-const socket = io('https://sleepy-island-33889.herokuapp.com/');
+const socket = io('https://warm-wave-96242.herokuapp.com/');
 
 socket.on('init', handleInit);
 socket.on('gameState', handleGameState);
